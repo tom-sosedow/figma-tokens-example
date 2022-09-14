6a1fb716412ab50c25c658f4a8e0e1479319caa3
tom-sosedow <tom-sosedow@users.noreply.github.com>
2022-09-14 10:35:24 +0000

    Update Tokens

---
+++ build/compose/StyleDictionaryColor.kt
@@ -0,0 +1,19 @@
+
+
+// Do not edit directly
+// Generated on Wed, 14 Sep 2022 10:35:23 GMT
+
+
+
+package StyleDictionaryColor;
+
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.*
+
+object StyleDictionaryColor {
+  val colorBlack = Color(0xff000000)
+  val colorBlue = Color(0xff001aff)
+  val colorRed = Color(0xffff0000)
+  val colorYellow = Color(0xffffc700)
+}
